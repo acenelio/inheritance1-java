b1ae05a9d5d8644289d47e3574bb0dfb517e8274
Nelio Alves <acenelio@gmail.com>
2018-06-02 21:49:49 -0300

    Initial implementation

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 0.0, 500.0);
+
+		System.out.println(account.getBalance());
+	}
+}
